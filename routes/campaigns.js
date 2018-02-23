bf93ab09d524addcb13b738915c9856922b3f51a
Helieber Sigesmundo <heliebersig@gmail.com>
2018-02-23 16:44:05 -0500

    Show single campaign view

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -6,9 +6,9 @@ const { ensureLoggedIn }  = require('connect-ensure-login');
 
 router.get('/new', (req, res) => {
     res.render('campaigns/new', { types: TYPES });
-  });
+});
 
-  router.post('/', ensureLoggedIn('/login'), (req, res, next) => {
+router.post('/', ensureLoggedIn('/login'), (req, res, next) => {
     const newCampaign = new Campaign({
       title: req.body.title,
       goal: req.body.goal,
@@ -27,5 +27,25 @@ router.get('/new', (req, res) => {
             res.redirect(`/campaigns/${newCampaign._id}`);
         }
     });
+});
+
+router.get('/:id', (req, res, next) => {
+    Campaign.findById(req.params.id, (err, campaign) => {
+      if (err){ return next(err); }
+  
+      campaign.populate('_creator', (err, campaign) => {
+        if (err){ return next(err); }
+        return res.render('campaigns/show', { campaign });
+      });
+    });
+});
+
+router.get('/:id/edit', ensureLoggedIn('/login'), (req, res, next) => {
+    Campaign.findById(req.params.id, (err, campaign) => {
+      if (err)       { return next(err) }
+      if (!campaign) { return next(new Error("404")) }
+      return res.render('campaigns/edit', { campaign, types: TYPES })
+    });
   });
+  
 module.exports = router;
